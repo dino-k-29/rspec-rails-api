6905c0c93de798f72b6fb0470776870cf45fa03f
Dino <dino@oat-bot.com>
2017-06-13 16:05:21 +0100

    Create and define factories for todos and items

---
+++ spec/factories/items.rb
@@ -0,0 +1,9 @@
+# By wrapping faker methods in a block, we ensure that faker generates dynamic data every time the factory is invoked. This way, we always have unique data.
+
+FactoryGirl.define do
+  factory :item do
+    name { Faker::StarWars.character }
+    done false
+    todo_id nil
+  end
+end
---
+++ spec/factories/todos.rb
@@ -0,0 +1,8 @@
+# By wrapping faker methods in a block, we ensure that faker generates dynamic data every time the factory is invoked. This way, we always have unique data.
+
+FactoryGirl.define do
+  factory :todo do
+    title { Faker::Lorem.word }
+    created_by { Faker::Number.number(10) }
+  end
+end
